2970ec5e63252d9a9ce74c8666a8caf99f828078
Jiang, Lin <linj2@illinois.edu>
2022-03-06 16:03:52 -0600

    add test type

---
+++ src/api/test-type-1/controllers/test-type-1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  test-type-1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::test-type-1.test-type-1');
---
+++ src/api/test-type-1/routes/test-type-1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-type-1 router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::test-type-1.test-type-1');
