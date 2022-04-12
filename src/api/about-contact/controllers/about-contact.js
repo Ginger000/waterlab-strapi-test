73ed409b9f5bf8dc17473ec78e7857f3c09da3b9
Jiang, Lin <linj2@illinois.edu>
2022-04-11 20:38:09 -0500

    add about contact

---
+++ src/api/about-contact/controllers/about-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  about-contact controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-contact.about-contact');
---
+++ src/api/about-contact/routes/about-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-contact router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::about-contact.about-contact');
